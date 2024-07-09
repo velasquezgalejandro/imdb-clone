3f951148ec638e0d71cef1fb76d082c009c598da
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-07-09 10:12:46 -0500

    creacion y actualizacion de footer y colores en tema

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -1,18 +1,29 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  typography: {
+    fontFamily: 'sans-serif',
+  },
   palette: {
     layout: {
-      main: '#0d0d15',
+      main: '#1a237e',
     },
     primary: {
       main: '#ff0000',
     },
     text: {
       main: '#000000',
-      white: '#fffff',
+      white: '#ffffff',
+      lightBlue: '#d1d3e5',
     },
   },
 });
+
+// #767bb2
+// #1a237e
+// #0d0d15
+// #050719
+// #0d47a1
+// #1a1a1a
 
 export default theme;
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,26 +1,74 @@
-import React from 'react';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+const Section = ({ title, children }) => {
+  return (
+    <Stack
+      rowGap={2}
+      sx={{
+        height: '100%',
+        width: 0.5,
+        p: 2,
+      }}
+    >
+      <Typography
+        align="center"
+        variant="h6"
+        sx={{ color: 'text.lightBlue', fontWeight: 'bold' }}
+      >
+        {title}
+      </Typography>
+      {children}
+    </Stack>
+  );
+};
 
 const Footer = () => {
+  const renderLink = (texto, link = '#') => {
+    return (
+      <Link
+        underline="hover"
+        target={'blank'}
+        href={link}
+        variant="body1"
+        sx={{ color: 'text.white' }}
+      >
+        {texto}
+      </Link>
+    );
+  };
+
   return (
     <Box
       sx={{
         display: 'flex',
         width: '100%',
-        height: 'fit-content',
-        bgcolor: '#85E3E9',
-        pt: 2,
+        bgcolor: 'layout.main',
+        py: 2,
         justifyContent: 'space-around',
-        alignItems: 'center',
       }}
     >
-      <Box sx={{ width: '40%' }}>Footer</Box>
+      <Section title={'primera sección'}>
+        <Stack>
+          {renderLink('enlace 1')}
+          {renderLink('enlace 2')}
+          {renderLink('enlace 3')}
+        </Stack>
+      </Section>
       <Divider
         orientation="vertical"
-        sx={{ color: 'Black', height: '200px' }}
+        sx={{ bgcolor: 'white', height: '200px' }}
       />
-      <Box sx={{ width: '40%' }}>Footer</Box>
+      <Section title={'segunda sección'}>
+        <Stack>
+          {renderLink('enlace 1')}
+          {renderLink('enlace 2')}
+          {renderLink('enlace 3')}
+        </Stack>
+      </Section>
     </Box>
   );
 };
